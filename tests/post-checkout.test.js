7d05ed2cf21c19262c4bb465b002e2dd483e4513
Aditi Mohanty <rheaditi@users.noreply.github.com>
2019-09-20 13:48:45 +0530

    Squash some commits
    Add record params script
    
    add checkout master example
    
    Add checkout commit example

---
+++ tests/post-checkout.test.js
@@ -0,0 +1,10 @@
+const example = [
+  {
+    name: 'git checkout bugfix/post-checkout-fails-when-checking_168659719',
+    params: '61c0ee6200a3f9faeb1e9c24755861582384c1a0 4c51aece308a74b4890ff67bbb2a0f75379ffd63 1',
+  },
+  {
+    name: 'git checkout 8da0bfffc570e8f2ad01cf91dfe1b124994fa068',
+    params: '8da0bfffc570e8f2ad01cf91dfe1b124994fa068 8da0bfffc570e8f2ad01cf91dfe1b124994fa068 1',
+  },
+];
